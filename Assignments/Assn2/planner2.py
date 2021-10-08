8bd5707f4f3b68f9094f7cac15a4f6d335bb77a5
vibhav011 <vibhav200601@gmail.com>
2021-10-08 15:45:51 +0530

    Implemented planner using list

--- Assignments/Assn2/planner2.py
+++ Assignments/Assn2/planner2.py
@@ -1,24 +1,16 @@
 import numpy as np
 import argparse
 import pulp as pl
-import sys
+
 class MDP:
     def __init__(self, states, actions):
         self.S = states
         self.A = actions
-        self.STR = [[[] for a in range(actions)] for s in range(states)]
-        self.TR = np.zeros((states, actions), dtype=float)
-        self.isNonTerminal = np.ones((states, actions), dtype=bool)
+        self.T = np.zeros((self.S, self.A, self.S))
+        self.R = np.zeros((self.S, self.A, self.S))
         self.gamma = 1.0
-    
-    def computeTerminal(self):
-        for s in range(self.S):
-            for a in range(self.A):
-                if len(self.STR[s][a]) == 0:
-                    return False
-                if self.STR[s][a][0][0] == s and self.STR[s][a][0][1] == 1.0 and self.STR[s][a][0][2] == 0.0:
-                    self.isNonTerminal[s, a] = False
-        return True
+        self.isNonTerminal = np.ones(self.S, dtype=bool)
+        self.numTerm = 0
 
 def constructMDP(path):
     f = open(path, 'r')
@@ -31,23 +23,15 @@ def constructMDP(path):
         t = int(end[i])
         if t < 0:
             continue
-        for a in range(A):
-            mdp.STR[t][a].append((t, 1.0, 0.0))
-
+        mdp.T[t, :, t] = 1.0
+        mdp.isNonTerminal[t] = False
+        mdp.numTerm += 1
+    
     line = f.readline().split()
     while line[0] == 'transition':
-        a, b, c, d, e = int(line[1]), int(line[2]), int(line[3]), float(line[4]), float(line[5])
-        mdp.STR[a][b].append((c, e, d))
-        mdp.TR[a][b] += d * e
+        mdp.R[int(line[1]), int(line[2]), int(line[3])] = float(line[4])
+        mdp.T[int(line[1]), int(line[2]), int(line[3])] = float(line[5])
         line = f.readline().split()
-    
-    for s in range(S):
-        for a in range(A):
-            mdp.STR[s][a].sort()
-    
-    if not mdp.computeTerminal():
-        print('ERROR: Some state-action pairs have no trasitions defined.')
-        sys.exit()
     
     mdp.gamma = float(f.readline().split()[1])
 
@@ -56,42 +40,25 @@ def constructMDP(path):
 
 def value_eval(mdp, P):
     P = P.astype(int)
-    isNonTerminal = mdp.isNonTerminal[np.arange(mdp.S), P]
-    k = np.sum(isNonTerminal)
-    
-    T1 = np.zeros((mdp.S,mdp.S))
-    for i in range(mdp.S):
-        if not isNonTerminal[i]:
-            continue
-        for t in mdp.STR[i][P[i]]:
-            s2 = t[0]
-            if not isNonTerminal[s2]:
-                continue
-            T1[i, s2] = t[1]
-    T1 = T1[:, isNonTerminal]
+    isNonTerminal = mdp.isNonTerminal
+    T1 = np.array([mdp.T[i, P[i], :] for i in range(mdp.S)])
     T1 = T1[isNonTerminal, :]
-
-    U = mdp.TR[np.arange(mdp.S), P]
-    U = U[isNonTerminal]
-
-    A = np.eye(k) - mdp.gamma * T1
+    R1 = np.array([mdp.R[i, P[i], :] for i in range(mdp.S)])
+    R1 = R1[isNonTerminal, :]
+    
+    U = np.sum(np.multiply(T1, R1), axis = 1)
+    T1 = T1[:, isNonTerminal]
+    A = np.eye(np.sum(isNonTerminal)) - mdp.gamma * T1
 
     V = np.zeros(mdp.S)
     V[isNonTerminal] = np.linalg.solve(A, U)
 
     return V
 
-def calcQ(mdp, s, a, V):
-    Q = 0
-    for t in mdp.STR[s][a]:
-        Q += t[1] * V[t[0]]
-    Q = mdp.TR[s,a] + mdp.gamma * Q
-    return Q
-
 def optPolicy(mdp, V):
     P = np.zeros(mdp.S, dtype=int)
     for s in range(mdp.S):
-        P[s] = np.argmax([calcQ(mdp, s, a, V) for a in range(mdp.A)])
+        P[s] = np.argmax(np.sum(np.multiply(mdp.T[s, :, :], mdp.R[s, :, :]), axis=1) + mdp.gamma * np.dot(mdp.T[s, :, :], V))
     return P
 
 def valueIter(mdp):
@@ -107,33 +74,29 @@ def HowardPI(mdp):
     P = np.zeros(mdp.S, dtype=int)
     V = np.zeros(mdp.S)
     optFound = False
+    TR = np.sum(np.multiply(mdp.T, mdp.R), axis=2)
 
     while not optFound:
         optFound = True
         V = value_eval(mdp, P)
-
         for s in range(mdp.S):
-            if not mdp.isNonTerminal[s, P[s]]:
-                continue
             for a in range(mdp.A):
                 if a == P[s]:
                     continue
-
-                if calcQ(mdp, s, a, V) > V[s]:
+                new_V = TR[s, a] + mdp.gamma * np.dot(mdp.T[s, a, :], V)
+                if new_V > V[s]:
                     P[s] = a
                     optFound = False
                     break
     return V, P
 
 def linearProgramming(mdp):
-    isNonTerminal = np.any(mdp.isNonTerminal, axis=1)
-
     Vs = [pl.LpVariable('V' + str(i)) for i in range(mdp.S)]
     prob = pl.LpProblem("MDPPlanning", pl.LpMinimize)
     for s in range(mdp.S):
         for a in range(mdp.A):
-            prob += Vs[s] >= pl.lpSum([t[1] * (t[2] + mdp.gamma*Vs[t[0]]) for t in mdp.STR[s][a]])
-        if not isNonTerminal[s]:
+            prob += Vs[s] >= pl.lpSum([mdp.T[s, a, s_] * (mdp.R[s, a, s_] + mdp.gamma*Vs[s_]) for s_ in range(mdp.S)])
+        if not mdp.isNonTerminal[s]:
             prob += Vs[s] == 0
     prob += pl.lpSum(Vs)
 
@@ -157,7 +120,7 @@ def main_function(mdp_path, alg):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--mdp', type=str, required=True)
-    parser.add_argument('--algorithm', type=str, default='hpi')
+    parser.add_argument('--algorithm', type=str, default='vi')
 
     args = parser.parse_args()
     
